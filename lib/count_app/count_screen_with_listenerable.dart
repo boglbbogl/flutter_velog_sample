88b42f92f2bf124cb19dd6741f09b5dd07d724c4
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-01-08 02:12:39 +0900

    create count app with value listenable

---
+++ lib/count_app/count_screen_with_listenerable.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_velog_sample/core/app_bar.dart';
+import 'package:flutter_velog_sample/count_app/count_screen_public_ui.dart';
+
+class CountScreenWithListenarable extends StatelessWidget {
+  const CountScreenWithListenarable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ValueNotifier<int> _count = ValueNotifier<int>(0);
+    ValueNotifier<int> _selectCount = ValueNotifier<int>(1);
+
+    return Scaffold(
+      appBar: appBar(title: 'Count App With State Ful'),
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: ValueListenableBuilder<int>(
+                        valueListenable: _count,
+                        builder: (
+                          BuildContext context,
+                          int count,
+                          Widget? child,
+                        ) {
+                          return Text(
+                            "$count",
+                            style: const TextStyle(
+                                fontSize: 60, fontWeight: FontWeight.bold),
+                          );
+                        }),
+                  )),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () =>
+                        _count.value = _count.value + _selectCount.value,
+                    child: const Icon(
+                      Icons.add_circle_outline,
+                      size: 40,
+                    ),
+                  ),
+                  const SizedBox(width: 24),
+                  GestureDetector(
+                    onTap: () =>
+                        _count.value = _count.value - _selectCount.value,
+                    child: const Icon(
+                      Icons.remove_circle_outline,
+                      size: 40,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 24),
+              GestureDetector(
+                onTap: () => _count.value = 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 3,
+                  height: 48,
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(71, 71, 71, 1),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: const Center(
+                    child: Text(
+                      'Reset',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+          Positioned(
+            top: 20,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: ValueListenableBuilder<int>(
+                    valueListenable: _selectCount,
+                    builder: (
+                      BuildContext context,
+                      int number,
+                      Widget? child,
+                    ) {
+                      return Column(
+                        children: [
+                          countAppSelectedCountBox(
+                              onTap: (number) => _selectCount.value = number,
+                              selectNumber: _selectCount.value,
+                              number: 1),
+                          countAppSelectedCountBox(
+                              onTap: (number) => _selectCount.value = number,
+                              selectNumber: _selectCount.value,
+                              number: 10),
+                          countAppSelectedCountBox(
+                              onTap: (number) => _selectCount.value = number,
+                              selectNumber: _selectCount.value,
+                              number: 20),
+                          countAppSelectedCountBox(
+                              onTap: (number) => _selectCount.value = number,
+                              selectNumber: _selectCount.value,
+                              number: 50),
+                          countAppSelectedCountBox(
+                              onTap: (number) => _selectCount.value = number,
+                              selectNumber: _selectCount.value,
+                              number: 100),
+                        ],
+                      );
+                    }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_velog_sample/core/app_theme.dart';
+import 'package:flutter_velog_sample/count_app/count_screen_with_listenerable.dart';
 import 'package:flutter_velog_sample/count_app/count_screen_with_stateful.dart';
 import 'package:flutter_velog_sample/count_app/provider/count_screen_with_provider.dart';
 import 'package:flutter_velog_sample/infinity_scroll/horizontal_infinity_scroll_screen.dart';
@@ -33,6 +34,8 @@ class App extends StatelessWidget {
         home: const InitialApp(),
         routes: {
           '/count/app/stateFul': (context) => const CountScreenWithStateFul(),
+          '/count/app/listenerable': (context) =>
+              const CountScreenWithListenarable(),
           '/count/app/provider': (context) => const CountScreenWithProvider(),
           '/scrollIndicator/vertical': (context) =>
               const VerticalIndicatorScreen(),
