51f66171fe0b34632dc066587ca2e9aae1ef9291
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-01-16 02:06:51 +0900

    create tab view

--- lib/tab_view/tab_view_tabbar_screen.dart
+++ lib/tab_view/tab_view_tabbar_screen.dart
@@ -20,12 +20,12 @@ class TabViewTabbarScreen extends StatelessWidget {
                           child: Wrap(
                         children: [
                           ...List.generate(
-                              10,
+                              13,
                               (index) => Container(
                                     height: 200,
                                     width:
-                                        MediaQuery.of(context).size.width / 10,
-                                    color: Colors.accents[index],
+                                        MediaQuery.of(context).size.width / 13,
+                                    color: Colors.accents[index % 15],
                                   )),
                         ],
                       ))),
@@ -33,30 +33,81 @@ class TabViewTabbarScreen extends StatelessWidget {
               }),
               body: Column(
                 children: [
-                  TabBar(tabs: [
-                    Tab(
-                      text: '1',
-                    ),
-                    Tab(
-                      text: '2',
-                    ),
-                    Tab(
-                      text: '3',
-                    ),
-                  ]),
+                  const TabBar(
+                      indicatorColor: Colors.white,
+                      labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                      indicatorWeight: 3,
+                      tabs: [
+                        Tab(
+                          text: 'List',
+                          height: 50,
+                        ),
+                        Tab(
+                          text: 'Grid',
+                          height: 50,
+                        ),
+                        Tab(
+                          text: 'Box',
+                          height: 50,
+                        ),
+                      ]),
                   Expanded(
                       child: TabBarView(children: [
+                    ListView.builder(
+                        key: const PageStorageKey("LIST_VIEW"),
+                        itemCount: 1000,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            width: MediaQuery.of(context).size.width,
+                            child: Center(
+                              child: Text(
+                                "List View $index",
+                                style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          );
+                        }),
+                    GridView.builder(
+                        key: const PageStorageKey("GRID_VIEW"),
+                        itemCount: 1000,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 12,
+                        ),
+                        itemBuilder: ((context, index) {
+                          return Container(
+                            color: Colors.accents[index % 15],
+                            child: Center(
+                                child: Text(
+                              "Grid View $index",
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                          );
+                        })),
                     Container(
                       width: 10,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 10,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 10,
-                      color: Colors.amber,
+                      color: const Color.fromRGBO(91, 91, 91, 1),
+                      child: const Center(
+                        child: Text(
+                          'Box',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 56,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
                   ]))
                 ],
