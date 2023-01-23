e47831958502bfb6be3e24d9fc82b007d1b8f57b
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-01-23 18:21:15 +0900

    update scroll view

--- lib/scroll_view/scroll_view_with_list_view_screen.dart
+++ lib/scroll_view/scroll_view_with_list_view_screen.dart
@@ -1,11 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_velog_sample/_core/app_bar.dart';
 
 class ScrollViewWithListViewScreen extends StatelessWidget {
   const ScrollViewWithListViewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBar(title: 'Scroll View With CustomScroll'),
+      body: ListView(
+        children: [
+          ...List.generate(
+              1000,
+              (index) => Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        "index : $index",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.accents[index % 15]),
+                      ),
+                    ),
+                  ))
+        ],
+      ),
+    );
   }
 }
