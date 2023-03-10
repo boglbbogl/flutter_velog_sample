97ac7de7a65f879d863820bcf777ce1554497b28
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-03-10 19:18:12 +0900

    update

--- lib/firebase/firestore/firebase_firestore_screen.dart
+++ lib/firebase/firestore/firebase_firestore_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_velog_sample/_core/app_bar.dart';
+import 'package:flutter_velog_sample/main.dart';
 
 class Test {
   final int id;
@@ -34,73 +38,79 @@ class FirebaseFirestoreScreen extends StatefulWidget {
 }
 
 class _FirebaseFirestoreScreenState extends State<FirebaseFirestoreScreen> {
-  Future<List<Test>> _fromCollection() async {
+  Stream<QuerySnapshot> _fromFirestore() {
     FirebaseFirestore _firestore = FirebaseFirestore.instance;
-    QuerySnapshot<Map<String, dynamic>> _snapshot =
-        await _firestore.collection("read_test").get();
-    List<Test> _result =
-        _snapshot.docs.map((e) => Test.fromJson(e.data())).toList();
-    return _result;
+
+    return _firestore.collection("read_test").snapshots();
+    // return
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(title: "Firebase Firestore"),
-      bottomNavigationBar: SafeArea(
-        child: GestureDetector(
-          onTap: () async {
-            FirebaseFirestore _firestore = FirebaseFirestore.instance;
-            for (int i = 0; i < 10; i++) {
-              await _firestore
-                  .collection("read_test")
-                  .doc()
-                  .set(Test(id: i, rank: i + 1, name: "Tyger $i").toJson());
-            }
-          },
-          child: Container(
-            width: 100,
-            height: 60,
-            color: Colors.black,
-            child: const Center(child: Text("CREATE")),
-          ),
-        ),
-      ),
-      body: FutureBuilder<List<Test>>(
-          future: _fromCollection(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: DefaultTextStyle(
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.accents[index % 15]),
-                        child: Column(
-                          children: [
-                            Text("ID : ${snapshot.data![index].id}"),
-                            Text("Name :  ${snapshot.data![index].name}"),
-                            Text("Rank : ${snapshot.data![index].name}"),
-                          ],
-                        ),
-                      ),
-                    );
-                  });
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.amber,
-                ),
-              );
-            } else {
-              return Container(
-                color: Colors.red,
-              );
-            }
-          }),
+      // bottomNavigationBar: SafeArea(
+      //   child: GestureDetector(
+      //     onTap: () async {
+      //       FirebaseFirestore _firestore = FirebaseFirestore.instance;
+      //       for (int i = 0; i < 10; i++) {
+      //         await _firestore
+      //             .collection("read_test")
+      //             .doc()
+      //             .set(Test(id: i, rank: i + 1, name: "Tyger $i").toJson());
+      //       }
+      //     },
+      //     child: Container(
+      //       width: 100,
+      //       height: 60,
+      //       color: Colors.black,
+      //       child: const Center(child: Text("CREATE")),
+      //     ),
+      //   ),
+      // ),
+      // body: StreamBuilder<Stream<QuerySnapshot>>(
+      //     stream: _fromFirestore(),
+      //     builder: (context, snapshot) {
+      //       if (snapshot.connectionState == ConnectionState.done) {
+      //         return ListView.separated(
+      //           itemCount: snapshot.data!.length,
+      //           itemBuilder: (context, index) {
+      //             return DefaultTextStyle(
+      //               style: TextStyle(
+      //                   fontWeight: FontWeight.bold,
+      //                   color: Colors.accents[index % 15]),
+      //               child: Column(
+      //                 children: [
+      //                   Text("ID : ${snapshot.data![index].id}"),
+      //                   Text("Name :  ${snapshot.data![index].rank}"),
+      //                   Text("Rank : ${snapshot.data![index].name}"),
+      //                 ],
+      //               ),
+      //             );
+      //           },
+      //           separatorBuilder: (BuildContext context, int index) {
+      //             return Padding(
+      //               padding: const EdgeInsets.symmetric(vertical: 12),
+      //               child: Container(
+      //                 height: 1,
+      //                 width: MediaQueryData.fromWindow(window).size.width,
+      //                 color: const Color.fromRGBO(91, 91, 91, 1),
+      //               ),
+      //             );
+      //           },
+      //         );
+      //       } else if (snapshot.connectionState == ConnectionState.waiting) {
+      //         return const Center(
+      //           child: CircularProgressIndicator(
+      //             color: Colors.amber,
+      //           ),
+      //         );
+      //       } else {
+      //         return Container(
+      //           color: Colors.red,
+      //         );
+      //       }
+      //     }),
     );
   }
 }
