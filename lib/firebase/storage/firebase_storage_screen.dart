a6dd3653e034971875a47897ac65581fb4d17d86
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-03-16 15:33:25 +0900

    create firebase storage

--- lib/firebase/storage/firebase_storage_screen.dart
+++ lib/firebase/storage/firebase_storage_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
-
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_velog_sample/_core/app_bar.dart';
 import 'package:flutter_velog_sample/main.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FirebaseStorageScreen extends StatefulWidget {
   const FirebaseStorageScreen({super.key});
@@ -13,16 +16,40 @@ class FirebaseStorageScreen extends StatefulWidget {
 }
 
 class _FirebaseStorageScreenState extends State<FirebaseStorageScreen> {
-  Future<void> _fetchNetworkToFile() async {
+  Future<File?> _fetchNetworkToFile() async {
     try {
-      http.Response _response = await http.get(Uri.parse("uri"));
+      http.Response _response = await http
+          .get(Uri.parse("https://picsum.photos/seed/picsum/200/300"));
       if (_response.statusCode == 200) {
-        //
+        Uint8List _unit8List = _response.bodyBytes;
+        ByteData _byte = ByteData.view(_unit8List.buffer);
+        Directory _systemDirectory = Directory.systemTemp;
+        File _file = await File('${_systemDirectory.path}/picsum').writeAsBytes(
+            _unit8List.buffer
+                .asUint8List(_byte.offsetInBytes, _byte.lengthInBytes));
+        return _file;
       } else {
-        //
+        return null;
       }
     } on HttpException catch (error) {
       logger.e(error);
+      return null;
+    }
+  }
+
+  Future<Uint8List?> _fetchNetworkToUint8List() async {
+    try {
+      http.Response _response =
+          await http.get(Uri.parse("https://picsum.photos/200/300/?blur"));
+      if (_response.statusCode == 200) {
+        Uint8List _unit8List = _response.bodyBytes;
+        return _unit8List;
+      } else {
+        return null;
+      }
+    } on HttpException catch (error) {
+      logger.e(error);
+      return null;
     }
   }
 
@@ -35,8 +62,29 @@ class _FirebaseStorageScreenState extends State<FirebaseStorageScreen> {
         children: [
           GestureDetector(
               onTap: () async {
-                logger.e("click");
-                _fetchNetworkToFile();
+                logger.e("message");
+                if (Platform.isIOS) {
+                  await Permission.photosAddOnly.request();
+                }
+                ImagePicker _picker = ImagePicker();
+                XFile? _pick =
+                    await _picker.pickImage(source: ImageSource.gallery);
+                if (_pick != null) {
+                  // File _file = File(_pick.path);
+                  Uint8List _bytes = await _pick.readAsBytes();
+                  FirebaseStorage.instance
+                      .ref("test/picker/test_picker")
+                      .putData(
+                        _bytes,
+                        SettableMetadata(
+                          contentType: "image/jpeg",
+                        ),
+                      );
+                }
+
+                // if (_image != null) {
+                //   logger.e(_image.name);
+                // }
               },
               child: Text("Click")),
         ],
