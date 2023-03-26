f7e123156f55ea3a7a473c5feca1e2f31843f7d5
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-03-27 00:48:40 +0900

    create apple watch - custom painter

---
+++ lib/custom_painter/bubble_chat/painter_bubble_chat_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_velog_sample/_core/app_bar.dart';
+
+class PainterBubbleChatScreen extends StatelessWidget {
+  const PainterBubbleChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(title: "Bubble Chat"),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQueryData.fromWindow(window).size.width,
+            height: MediaQueryData.fromWindow(window).size.width,
+            color: Colors.amber,
+            child: CustomPaint(
+              painter: _BubbleChat(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BubbleChat extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
