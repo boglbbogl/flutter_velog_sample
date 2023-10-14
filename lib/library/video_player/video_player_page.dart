27d109b6e056179b7d90220df4c3a389502646f6
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-10-15 02:01:34 +0900

    video player

--- lib/library/video_player/video_player_page.dart
+++ lib/library/video_player/video_player_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_velog_sample/_core/app_bar.dart';
-import 'package:flutter_velog_sample/main.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerPage extends StatefulWidget {
@@ -12,28 +12,38 @@ class VideoPlayerPage extends StatefulWidget {
 
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
   late VideoPlayerController controller;
-  // File? videoFile;
+  double? aspectRatio;
+  double progress = 0;
+  Duration position = Duration.zero;
+
   @override
   void initState() {
     super.initState();
     controller = VideoPlayerController.asset("assets/videos/test_video_2.mp4")
       ..initialize();
-    // controller = VideoPlayerController.networkUrl(Uri.parse(
-    //     "https://firebasestorage.googleapis.com/v0/b/flutter-velog-sample.appspot.com/o/KEYCUTstock_preview_UK14030.mp4?alt=media&token=93f38e19-41f4-4aeb-b6bc-2df9ed9d135a&_gl=1*1o74ov2*_ga*MTMwNTY4NTMwLjE2ODAyNDU1OTc.*_ga_CW55HF8NVT*MTY5NzE4MTg5Ni4yNjkuMS4xNjk3MTgyNDQyLjU3LjAuMA"))
-    // ..initialize();
     controller.setPlaybackSpeed(1);
-
-    controller.play();
-
-    controller.setVolume(0.4);
+    played();
 
     controller.addListener(() async {
-      // logger.e(controller.value.aspectRatio);
-      // logger.e(controller.value.duration);
-      controller.seekTo(Duration(seconds: 17));
-      logger.e(controller.value.position);
-      logger.e(controller.value.volume);
+      int max = controller.value.duration.inSeconds;
+      setState(() {
+        aspectRatio = controller.value.aspectRatio;
+        position = controller.value.position;
+        progress = (position.inSeconds / max * 100).isNaN
+            ? 0
+            : position.inSeconds / max * 100;
+      });
     });
+  }
+
+  void played() => controller.play();
+
+  void stoped() => controller.pause();
+
+  void seekTo(int value) {
+    int add = position.inSeconds + value;
+
+    controller.seekTo(Duration(seconds: add < 0 ? 0 : add));
   }
 
   @override
@@ -46,15 +56,112 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(title: "Video Player"),
-      body: Column(
+      body: Stack(
         children: [
-          Stack(
-            children: [
-              AspectRatio(
-                  aspectRatio: controller.value.aspectRatio,
-                  child: VideoPlayer(controller)),
-            ],
-          ),
+          if (aspectRatio != null) ...[
+            Center(
+              child: Padding(
+                padding: EdgeInsets.only(
+                    bottom:
+                        MediaQuery.of(context).padding.top + kToolbarHeight),
+                child: AspectRatio(
+                    aspectRatio: aspectRatio!, child: VideoPlayer(controller)),
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        HapticFeedback.mediumImpact();
+                        seekTo(-10);
+                      },
+                      child: const SizedBox(
+                        width: 30,
+                        child: Icon(
+                          Icons.replay_10_rounded,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        HapticFeedback.mediumImpact();
+                        if (controller.value.isPlaying) {
+                          stoped();
+                        } else {
+                          played();
+                        }
+                      },
+                      child: SizedBox(
+                        width: 30,
+                        child: Icon(
+                          controller.value.isPlaying
+                              ? Icons.stop
+                              : Icons.play_arrow_rounded,
+                          size: 32,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        HapticFeedback.mediumImpact();
+                        seekTo(10);
+                      },
+                      child: const SizedBox(
+                        width: 30,
+                        child: Icon(
+                          Icons.forward_10_rounded,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    SizedBox(
+                        width: 30,
+                        child: Text(
+                          controller.value.position.toString().substring(2, 7),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 14),
+                        )),
+                    Stack(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 4),
+                          height: 6,
+                          width: MediaQuery.of(context).size.width - 206,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: const Color.fromRGBO(135, 135, 135, 1),
+                          ),
+                        ),
+                        AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          margin: const EdgeInsets.symmetric(horizontal: 4),
+                          height: 6,
+                          width: (MediaQuery.of(context).size.width - 206) *
+                              (progress / 100),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: const Color.fromRGBO(215, 215, 215, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                        width: 30,
+                        child: Text(
+                          controller.value.duration.toString().substring(2, 7),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 14),
+                        )),
+                  ],
+                ),
+              ),
+            )
+          ]
         ],
       ),
     );
